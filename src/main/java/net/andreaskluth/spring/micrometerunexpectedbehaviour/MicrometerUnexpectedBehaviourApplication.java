01bcd2338ac887d286786ffa777b4690ef50fab9
cdiem4558k <chase_diem@cable.comcast.com>
2019-10-28 10:09:46 -0400

    invalid json response

--- src/main/java/net/andreaskluth/spring/micrometerunexpectedbehaviour/MicrometerUnexpectedBehaviourApplication.java
+++ src/main/java/net/andreaskluth/spring/micrometerunexpectedbehaviour/MicrometerUnexpectedBehaviourApplication.java
@@ -6,32 +6,57 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MicrometerUnexpectedBehaviourApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(MicrometerUnexpectedBehaviourApplication.class, args);
-  }
-
-  @org.springframework.context.annotation.Configuration
-  public static class Configuration {
-
-    @Bean
-    public TimedAspect timedAspect(MeterRegistry meterRegistry) {
-      return new TimedAspect(meterRegistry);
+    public static void main(String[] args) {
+        SpringApplication.run(MicrometerUnexpectedBehaviourApplication.class, args);
     }
-  }
 
-  @RestController
-  public static class DemoController {
+    @org.springframework.context.annotation.Configuration
+    public static class Configuration {
 
-    @Timed("demo_controller_omg")
-    @GetMapping("/")
-    public String omg() {
-      return "test";
+        @Bean
+        public TimedAspect timedAspect(MeterRegistry meterRegistry) {
+            return new TimedAspect(meterRegistry);
+        }
     }
-  }
+
+    @RestController
+    public static class DemoController {
+
+        @Timed("demo_controller_omg")
+        @GetMapping("/")
+        public String omg() {
+            return "test";
+        }
+
+
+
+        @Timed("omgResponse")
+        @GetMapping("/response")
+        public ResponseEntity<ResponseObject> omgResponse() {
+            ResponseObject r = new ResponseObject();
+            r.setResponse("test");
+            return new ResponseEntity(r, HttpStatus.OK);
+        }
+    }
+
+    public static class ResponseObject {
+        private String response;
+
+        public String getResponse() {
+            return response;
+        }
+
+        public void setResponse(String response) {
+            this.response = response;
+        }
+    }
 }
